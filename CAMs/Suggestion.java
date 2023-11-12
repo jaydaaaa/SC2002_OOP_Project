a532da29f4bb46df7098db27cf3fd561e965d24d
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-12 16:36:13 +0800

    Update Suggestion.java

--- CAMs/Suggestion.java
+++ CAMs/Suggestion.java
@@ -4,4 +4,22 @@ public class Suggestion {
 	private String suggestedBy;
 	private boolean status;
 
+	public Suggestion(String suggestionText, String suggestedBy, boolean status) {
+        	this.suggestionText = suggestionText;
+        	this.suggestedBy = suggestedBy;
+        	this.status = status;
+	}
+
+	public String getSuggestionText() {
+        	return suggestionText;
+    	}
+
+	public String getSuggestedBy() {
+        	return suggestedBy;
+    	}
+
+	public boolean getStatus() {
+        	return status;
+    	}
+
 }

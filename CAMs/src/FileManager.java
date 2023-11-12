147804187a5f1864ef180078d86bee7c1b1aa6f7
jaydaaaa <foojayda@gmail.com>
2023-11-13 00:48:13 +0800

    Added code for reading csv

--- CAMs/src/FileManager.java
+++ CAMs/src/FileManager.java
@@ -1,13 +1,36 @@
+import java.io.*;
+import java.util.*;
+
 public class FileManager {
+    private String filePath;
 
-	public void readFiles() {
-		// TODO - implement FileManager.readFiles
-		throw new UnsupportedOperationException();
-	}
+    public FileManager(String filePath) {
+        this.filePath = filePath;
+    }
 
-	public void writeFiles() {
-		// TODO - implement FileManager.writeFiles
-		throw new UnsupportedOperationException();
-	}
+    public List<User> readFiles() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine(); // First line is header, skip it.
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                users.add(new User(values[0], values[1].split("@")[0].trim()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
 
+    public void writeFiles(List<String[]> data) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (String[] row : data) {
+                bw.write(String.join(",", row));
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

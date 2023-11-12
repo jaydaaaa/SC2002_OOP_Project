6da0338bddeb4076d8f12e5d12869779c0b8b7ca
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-12 16:33:38 +0800

    Update Staff.java

--- CAMs/Staff.java
+++ CAMs/Staff.java
@@ -1,5 +1,14 @@
 public class Staff extends User {
 
-	private List<Camp> createdCamps;
+    private List<Camp> createdCamps;
 
+    public Staff(String userID, String faculty, List<Camp> createdCamps) {
+        super(userID, faculty);
+        this.createdCamps = createdCamps;
+    }
+
+    public List<Camp> getCreatedCamps() {
+        return createdCamps;
+    }
+	
 }

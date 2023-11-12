a984e226005e28201623c3594f62f513a413ce56
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-12 17:21:17 +0800

    Update LoginAuthentication.java

--- CAMs/src/LoginAuthentication.java
+++ CAMs/src/LoginAuthentication.java
@@ -1,29 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginAuthentication {
 
-	private Dictionary<userID, password> password;
+    private Map<String, String> passwordMap;
 
-	/**
-	 * 
-	 * @param userID
-	 * @param password
-	 */
-	public void register(String userID, String password) {
-		// TODO - implement LoginAuthentication.register
-		throw new UnsupportedOperationException();
-	}
+    public LoginAuthentication() {
+        this.passwordMap = new HashMap<>();
+    }
 
-	/**
-	 * 
-	 * @param newPassword
-	 */
-	public void changePassword(String newPassword) {
-		// TODO - implement LoginAuthentication.changePassword
-		throw new UnsupportedOperationException();
-	}
+    public void register(String userID, String password) {
+        if (!passwordMap.containsKey(userID)) {
+            passwordMap.put(userID, password);
+            System.out.println("Registration successful for user: " + userID);
+        } else {
+            System.out.println("User ID already exists. Please choose another one.");
+        }
+    }
 
-	public boolean login() {
-		// TODO - implement LoginAuthentication.login
-		throw new UnsupportedOperationException();
-	}
+    public void changePassword(String userID, String newPassword) {
+        if (passwordMap.containsKey(userID)) {
+            passwordMap.put(userID, newPassword);
+            System.out.println("Password changed successfully for user: " + userID);
+        } else {
+            System.out.println("User not found. Password change failed.");
+        }
+    }
 
+    public boolean login(String userID, String password) {
+        return passwordMap.containsKey(userID) && passwordMap.get(userID).equals(password);
+    }
 }

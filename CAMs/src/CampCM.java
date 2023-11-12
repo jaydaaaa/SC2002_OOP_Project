7994dd1bf0e91aa95f462002530cc0cc31b4e086
KzRihan <127655609+KzRihan@users.noreply.github.com>
2023-11-13 01:10:58 +0800

    Update CampCM.java

--- CAMs/src/CampCM.java
+++ CAMs/src/CampCM.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
-public class CampCommMem extends Student {
+public class CampCM extends Student {
 
 	private int myPoints;
 	private List<Suggestion> mySuggestions;
 	
-	public CampCommMem(String userID, String faculty, List<Enquiry> myEnquiries, List<Camp> myCamps, int myPoints, List<Suggestion> mySuggestions) {
+	public CampCM(String userID, String faculty, List<Enquiry> myEnquiries, List<Camp> myCamps, int myPoints, List<Suggestion> mySuggestions) {
         super(userID, faculty, myEnquiries, myCamps);
         this.myPoints = myPoints;
         this.mySuggestions = mySuggestions;

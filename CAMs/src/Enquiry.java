c4cab9cfc5b8a64a9e29c482cee2ed3a223d658d
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-12 16:45:28 +0800

    Update Enquiry.java

--- CAMs/src/Enquiry.java
+++ CAMs/src/Enquiry.java
@@ -1,9 +1,37 @@
 public class Enquiry {
 
-	private String enquiryText;
-	private String enquiryBy;
-	private String replyText;
-	private String replyBy;
-	private boolean status;
+    private String enquiryText;
+    private String enquiryBy;
+    private String replyText;
+    private String replyBy;
+    private boolean status;
+
+    public Enquiry(String enquiryText, String enquiryBy, String replyText, String replyBy, boolean status) {
+        this.enquiryText = enquiryText;
+        this.enquiryBy = enquiryBy;
+        this.replyText = replyText;
+        this.replyBy = replyBy;
+        this.status = status;
+    }
+
+    public String getEnquiryText() {
+        return enquiryText;
+    }
+
+    public String getEnquiryBy() {
+        return enquiryBy;
+    }
+
+    public String getReplyText() {
+        return replyText;
+    }
+
+    public String getReplyBy() {
+        return replyBy;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
 
 }

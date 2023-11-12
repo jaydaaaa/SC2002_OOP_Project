960a3b95042508a01b4aa2edeafd0567a0c1e6a8
Chia Yi Xuan <127966901+Yixuan1012@users.noreply.github.com>
2023-11-12 17:20:10 +0800

    CampCommMem.java

--- CAMs/src/CampCommMem.java
+++ CAMs/src/CampCommMem.java
@@ -1,6 +1,19 @@
+import java.util.List;
+
 public class CampCommMem extends Student {
 
 	private int myPoints;
 	private List<Suggestion> mySuggestions;
-
+	
+	public CampCommMem(String userID, String faculty, List<Enquiry> myEnquiries, List<Camp> myCamps, int myPoints, List<Suggestion> mySuggestions) {
+        super(userID, faculty, myEnquiries, myCamps);
+        this.myPoints = myPoints;
+        this.mySuggestions = mySuggestions;
+    }
+	public int getMyPoints() {
+		return this.myPoints;
+	}
+	public List<Suggestion> getMySuggestions(){
+		return this.mySuggestions;
+	}
 }

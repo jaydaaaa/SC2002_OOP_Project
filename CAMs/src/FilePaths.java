6ad0a1e1a89c3f079ffccaf32b0958106f67969a
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-13 19:11:49 +0800

    Update FilePaths.java

--- CAMs/src/FilePaths.java
+++ CAMs/src/FilePaths.java
@@ -1,8 +1,14 @@
 public class FilePaths {
 
-	public int STUDENT_LIST;
-	public int STAFF_LIST;
-	public int CAMP_LIST;
-	public int CAMP_COMM_MEM_LIST;
+    public String STUDENT_LIST;
+    public String STAFF_LIST;
+    public String CAMP_LIST;
+    public String CAMP_COMM_MEM_LIST;
 
+    public FilePaths(String studentList, String staffList, String campList, String campCommMemList) {
+        this.STUDENT_LIST = studentList;
+        this.STAFF_LIST = staffList;
+        this.CAMP_LIST = campList;
+        this.CAMP_COMM_MEM_LIST = campCommMemList;
+    }
 }

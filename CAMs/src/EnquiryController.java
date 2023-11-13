a7c8dbdc59975b73672486ffcb9ee3733c2fb6bb
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-13 16:18:34 +0800

    Update EnquiryController.java

--- CAMs/src/EnquiryController.java
+++ CAMs/src/EnquiryController.java
@@ -1,41 +1,32 @@
 public class EnquiryController {
 
-	/**
-	 * 
-	 * @param student
-	 * @param enquiry
-	 */
-	public void editEnquiry(String student, String enquiry) {
-		// TODO - implement EnquiryController.editEnquiry
-		throw new UnsupportedOperationException();
-	}
+    public void editEnquiry(Student student, Enquiry enquiry, String newEnquiryText) {
+        if(student.getMyEnquiries().contains(enquiry)) {
+            enquiry.setEnquiryText(newEnquiryText);
+        } else {
+            System.out.println("Enquiry not found for the given student");
+        }
+    }
 
-	/**
-	 * 
-	 * @param student
-	 */
-	public void deleteEnquiry(String student) {
-		// TODO - implement EnquiryController.deleteEnquiry
-		throw new UnsupportedOperationException();
-	}
+    public void deleteEnquiry(Student student, Enquiry enquiry) {
+        if(student.getMyEnquiries().contains(enquiry)) {
+            student.getMyEnquiries().remove(enquiry);
+        } else {
+            System.out.println("Enquiry not found for the given student");
+        }
+    }
 
-	/**
-	 * 
-	 * @param String
-	 */
-	public void addEnquiry(int String) {
-		// TODO - implement EnquiryController.addEnquiry
-		throw new UnsupportedOperationException();
-	}
+    public void addEnquiry(Student student, String enquiryText) {
+        Enquiry newEnquiry = new Enquiry(enquiryText, student.getUserID(), "", "", false);
+        student.getMyEnquiries().add(newEnquiry);
+    }
 
-	/**
-	 * 
-	 * @param student
-	 * @param enquiry
-	 */
-	public void replyEnquiry(String student, String enquiry) {
-		// TODO - implement EnquiryController.replyEnquiry
-		throw new UnsupportedOperationException();
-	}
-
+    public void replyEnquiry(Student student, Enquiry enquiry, String replyText) {
+        if(student.getMyEnquiries().contains(enquiry)) {
+            enquiry.setReplyText(replyText);
+            enquiry.setStatus(true);
+        } else {
+            System.out.println("Enquiry not found for the given student");
+        }
+    }
 }

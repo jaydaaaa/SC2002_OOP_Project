68ce7d6e403eba22f6fee73e9e2007ef0e2d91b0
Chan Wei Ting <91749481+ChanWeiTing@users.noreply.github.com>
2023-11-13 20:40:26 +0800

    Update Test file.java

--- CAMs/src/Test file.java
+++ CAMs/src/Test file.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Your existing classes and methods...
 
 public class Main{
@@ -6,29 +9,33 @@ public class Main{
         List<Camp> createdCamps = new ArrayList<>();
 
         // Create Camp 1
-        List<Student> attendees1 = new ArrayList<>();
-        attendees1.add(new Student("Wei Ting", "Alice"));
-        attendees1.add(new Student("Yi Xuan", "Bob"));
-        List<CampCommMem> committeeMembers1 = new ArrayList<>();
-        committeeMembers1.add(new CampCommMem("moon", "Charlie"));
-        committeeMembers1.add(new CampCommMem("sun", "Diana"));
         List<Suggestion> suggestion1 = new ArrayList<>();
         List<Enquiry> enquiry1 = new ArrayList<>();
+        List<Enquiry> enquiry2  = new ArrayList<>();
+        List<Student> attendees1 = new ArrayList<>();
+        List<Camp> myCamp1 = new ArrayList<>();
+        List<Camp> myCamp2 = new ArrayList<>();
+        attendees1.add(new Student("Wei Ting", "Alice", enquiry1, myCamp1 ));
+        attendees1.add(new Student("Yi Xuan", "Bob", enquiry2, myCamp2 ));
+        List<CampCommMem> committeeMembers1 = new ArrayList<>();
+        committeeMembers1.add(new CampCommMem("moon", "Charlie", enquiry1, myCamp1));
+        committeeMembers1.add(new CampCommMem("sun", "Diana", enquiry2, myCamp2));
+        
 
         Camp camp1 = new Camp("Summer Camp", "2023-07-01", "2023-07-15", "Group A", "Location A", "Description A", 100, "Staff1", 10, true, attendees1, committeeMembers1, enquiry1, suggestion1);
         createdCamps.add(camp1);
 
         // Create Camp 2
         List<Student> attendees2 = new ArrayList<>();
-        attendees2.add(new Student("Poop", "Eve"));
-        attendees2.add(new Student("Pee", "Frank"));
+        attendees2.add(new Student("Poop", "SCSE", enquiry1, myCamp1));
+        attendees2.add(new Student("Pee", "NBS", enquiry2, myCamp1));
         List<CampCommMem> committeeMembers2 = new ArrayList<>();
-        committeeMembers2.add(new CampCommMem("Bean", "Grace"));
-        committeeMembers2.add(new CampCommMem("Boon", "Harry"));
+        committeeMembers2.add(new CampCommMem("Bean", "Grace", enquiry1, myCamp1));
+        committeeMembers2.add(new CampCommMem("Boon", "Harry", enquiry2, myCamp2));
         List<Suggestion> suggestion2 = new ArrayList<>();
-        List<Enquiry> enquiry2 = new ArrayList<>();
+        List<Enquiry> enquiry3 = new ArrayList<>();
 
-        Camp camp2 = new Camp("Winter Camp", "2023-12-01", "2023-12-1", "Group B", "Location B", "Description B", 150, "Staff2", 15, true, attendees2, committeeMembers2,enquiry2,suggestion2);
+        Camp camp2 = new Camp("Winter Camp", "2023-12-01", "2023-12-1", "Group B", "Location B", "Description B", 150, "Staff2", 15, true, attendees2, committeeMembers2,enquiry3,suggestion2);
         createdCamps.add(camp2);
 
         // Call the method from the StaffController class to generate the report

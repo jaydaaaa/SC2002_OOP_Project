c86101889369186e810e435b8067232134357dc7
Chan Wei Ting <91749481+ChanWeiTing@users.noreply.github.com>
2023-11-15 01:07:09 +0800

    Update BaseBoundary.java

--- CAMs/src/boundary/BaseBoundary.java
+++ CAMs/src/boundary/BaseBoundary.java
@@ -1,5 +1,62 @@
 package boundary;
+import entity.CentralManager;
+import utils.Base;
+import utils.BaseInterface;
+import utils.Input;
 
-public class BaseBoundary {
-    
+public class BaseBoundary extends Base implements BaseInterface {
+    CentralManager centralManager;
+    Input input;
+
+    public BaseBoundary(CentralManager centralManager) {
+        super(centralManager);
+        this.centralManager = centralManager;
+        this.input = centralManager.getInput();
+    }
+
+    @Override
+    public CentralManager getCentralManager() {
+        return this.centralManager;
+    }
+
+    public Integer getInt(String prompt) {
+        return this.input.getInt(prompt);
+    }
+
+    public Double getDouble(String prompt) {
+        return this.input.getDouble(prompt);
+    }
+
+    public String getLine(String prompt) {
+        return this.input.getLine(prompt);
+    }
+
+    // get Boundaries
+    public CampBoundary getCampBoundary() {
+        return this.getCentralManager().getCampBoundary();
+    }
+
+    public CampCMBoundary getCampCMBoundary() {
+        return this.getCentralManager().getCampCMBoundary();
+    }
+
+    public EnquiryBoundary getEnquiryBoundary() {
+        return this.getCentralManager().getEnquiryBoundary();
+    }
+
+    public StaffBoundary getStaffBoundary() {
+        return this.getCentralManager().getStaffBoundary();
+    }
+
+    public StudentBoundary getStudentBoundary() {
+        return this.getCentralManager().getStudentBoundary();
+    }
+
+    public SuggestionBoundary getSuggestionBoundary() {
+        return this.getCentralManager().getSuggestionBoundary();
+    }
+
+    public UserBoundary getUserBoundary() {
+        return this.getCentralManager().getUserBoundary();
+    }
 }

dad01545214c01dfc7f62e7ec5d635ef9e2fbd8c
Chan Wei Ting <91749481+ChanWeiTing@users.noreply.github.com>
2023-11-18 01:04:02 +0800

    Update UserController.java

--- CAMs/src/controller/UserController.java
+++ CAMs/src/controller/UserController.java
@@ -1,5 +1,28 @@
 package controller;
+import boundary.BaseBoundary;
+import entity.CentralManager;
+import entity.User;
+import java.util.ArrayList;
+import java.util.Objects;
+public class UserController extends BaseController{
+    ArrayList<User> users;
+    User currentUser;
 
-public class UserController {
-    
+    public UserController(CentralManager centralManager) {
+        super(centralManager);
+        this.users = this.centralManager.getMasterUsers();
+    }
+
+    public void setcurrentUser(String userID) {
+        this.currentUser = this.getUserID(userID);
+    }
+
+    public User getUserID(String userID) {
+        for (User user: this.users) {
+            if (Objects.equals(user.getUserId(), userID)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }

90764de1939f74b7b4bbd033006921b6baae32a5
jaydaaaa <foojayda@gmail.com>
2023-11-19 00:35:03 +0800

    Edited Staff.java

--- CAMs/src/entity/Staff.java
+++ CAMs/src/entity/Staff.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Staff extends User{
     private List<Camp> createdCamps;
 
-    public Staff(String userID, String faculty) {
-        super(userID, faculty);
+    public Staff(String name, String userID, String email, String faculty, String password, String type) {
+        super(name, userID, email, faculty, password, type);
         createdCamps = new ArrayList<>();
     }
 

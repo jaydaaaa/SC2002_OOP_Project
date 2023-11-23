ab40372e595a3860f4d6f1885b3aeabd9524903b
Chan Wei Ting <91749481+ChanWeiTing@users.noreply.github.com>
2023-11-23 15:48:02 +0800

    Update Staff.java

--- CAMs/src/entity/Staff.java
+++ CAMs/src/entity/Staff.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Staff extends User{
-    private List<Camp> createdCamps;
+    private ArrayList<Camp> createdCamps;
 
     public Staff(String name, String userID, String email, String faculty, String password, String type) {
         super(name, userID, email, faculty, password, type);
         createdCamps = new ArrayList<>();
     }
 
-    public List<Camp> getCamps() {
+    public ArrayList<Camp> getCamps() {
         return createdCamps;
     }
+    
+    // Added addCamps function
+    public void addCamps(Camp newCamp) { this.createdCamps.add(newCamp);}
 }

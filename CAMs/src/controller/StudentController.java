910ecd1271b82ed484f39bdf179962ee592d9411
Chia Yi Xuan <127966901+Yixuan1012@users.noreply.github.com>
2023-11-26 21:45:28 +0800

    added javadocs

--- CAMs/src/controller/StudentController.java
+++ CAMs/src/controller/StudentController.java
@@ -6,23 +6,56 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * The StudentController class is a controller for managing Student entities.
+ * It extends the UserController class and provides additional functionality specific to student members.
+ * @author Group 2
+ * @since 2023-11-26
+ */
 public class StudentController extends UserController{
+	
+    /**
+     * Constructs a StudentController with the specified CentralManager.
+     *
+     * @param centralManager The CentralManager instance to associate with the StudentController.
+     */
 	public StudentController(CentralManager centralManager){
         super(centralManager);
     }
 
+	/**
+     * Gets the current student associated with this controller.
+     *
+     * @return The current Student member.
+     */
 	public Student getCurrentStudent() {
         return (Student) this.currentUser;
     }
 
+	 /**
+     * Sets a new password for the current student member.
+     *
+     * @param newPassword The new password to set.
+     */
 	public void setPassword(String newPassword){
         this.getCurrentStudent().setPassword(newPassword);
     }
 
+	 /**
+     * Retrieves the faculty of the current student.
+     *
+     * @return The faculty of the current student.
+     */
 	public String getFaculty() {
 		return this.getCurrentStudent().getFaculty();
 	}
 
+	/**
+     * Retrieves a student member by their student ID.
+     *
+     * @param studentID The student ID of the desired student member.
+     * @return The Student member with the specified student ID, or null if not found.
+     */
 	public Student getStudentByID(String studentID) {
 		for (User user: this.getCentralManager().getMasterUsers()) {
 			if (user.getUserID().equals(studentID)) {
@@ -32,6 +65,13 @@ public class StudentController extends UserController{
 		return null;
 	}
 
+	  /**
+     * Checks if the dates of two camps overlap.
+     *
+     * @param camp1 The first camp.
+     * @param camp2 The second camp.
+     * @return True if the dates overlap, false otherwise.
+     */
 	public boolean doesClash(Camp camp1, Camp camp2) {
 		Integer start1, start2, end1, end2;
 		ArrayList<Integer> dates1 = camp1.getDates();
@@ -47,6 +87,13 @@ public class StudentController extends UserController{
 		}
 	}
 
+	/**
+     * Registers the current student for a camp.
+     *
+     * @param campID       The ID of the camp to register for.
+     * @param choiceCampComm True if the student wants to join as CampComm, false otherwise.
+     * @return 1 if registration is successful, 0 if there is a clash, -1 if the camp is full, -2 if the registration deadline has passed, -3 if no empty CampComm slots.
+     */
 	public int registerForCamp(String campID, boolean choiceCampComm) {
 		// Find the camp
 		Camp newCamp = this.getCampController().getCampByID(campID);
@@ -101,20 +148,42 @@ public class StudentController extends UserController{
 		return 1;
 	}
 
+	/**
+     * Withdraws the current student from a camp.
+     *
+     * @param campID The ID of the camp to withdraw from.
+     * @return The result of the withdrawal operation: 1 if successful, 0 if the student is a committee member, -1 if the student is not an attendee.
+     */
 	public int withdrawFromCamp(String campID) {
 		// Find the camp
 		// Remove the student from the camp's attendees
 		return this.getCampController().removeAttendee(this.getCurrentStudent(), campID);
 	}
 
+	/**
+     * Retrieves a list of camps the current student is participating in.
+     *
+     * @return An ArrayList of camps the current student is participating in.
+     */
 	public ArrayList<Camp> getAttendedCamps() {
 		return this.getCampController().getAttendedCamps(this.getCurrentStudent().getUserID());
 	}
 
+	/**
+     * Retrieves a list of enquiries sent by the current student.
+     *
+     * @return An ArrayList of enquiries sent by the current student.
+     */
 	public ArrayList<Enquiry> getMyEnquiries() {
 		return this.getEnquiryController().findEnquiriesBySender(this.getCurrentStudent().getUserID());
 	}
 
+	 /**
+     * Retrieves a list of students participating in the specified camps.
+     *
+     * @param camps The list of camps for which attendees are to be retrieved.
+     * @return An ArrayList of students participating in the specified camps.
+     */
 	public ArrayList<Student> getAttendeesFromCamps(ArrayList<Camp> camps) {
 		ArrayList<Student> attendees = new ArrayList<>();
 		for (Camp camp: camps) {

77a22cad13fd9127c03b5d2dcdc354a1716fdafa
Chia Yi Xuan <127966901+Yixuan1012@users.noreply.github.com>
2023-11-26 22:27:22 +0800

    javadocs

--- CAMs/src/utils/BaseInterface.java
+++ CAMs/src/utils/BaseInterface.java
@@ -2,6 +2,18 @@ package utils;
 
 import entity.CentralManager;
 
+/**
+ * The BaseInterface interface defines a contract for classes that provide access to a CentralManager.
+ * Classes implementing this interface are expected to provide an implementation for retrieving the central manager.
+ * @author Group 2
+ * @since 2023-11-26
+ */
 public interface BaseInterface {
+	
+	/**
+     * Gets the CentralManager associated with the implementing class.
+     *
+     * @return The CentralManager instance associated with the implementing class.
+     */
     public CentralManager getCentralManager();
 }

094b89b350084f1d41379750621292750ee79aa0
Chia Yi Xuan <127966901+Yixuan1012@users.noreply.github.com>
2023-11-26 22:37:35 +0800

    javadocs

--- CAMs/src/utils/Input.java
+++ CAMs/src/utils/Input.java
@@ -1,5 +1,4 @@
 package utils;
-
 import java.text.ParseException;
 import java.util.Date;
 import java.util.InputMismatchException;
@@ -7,34 +6,38 @@ import java.util.Objects;
 import java.util.Scanner;
 
 /**
- * An App.Input Object
- * <p>
- * An <code>App.Input</code> object contains methods to ask for user input
- * </p>
+ * The Input class represents an object for handling user input operations.
+ * It provides methods to interactively ask for various types of user input.
  */
 public class Input {
     /**
-     * Object of class Scanner
+     * Scanner object for reading user input.
      */
     Scanner sc;
 
     /**
-     * Creates a new object of class App.Input
-     * @param sc Scanner object
+     * Constructs a new Input object with the specified Scanner.
+     *
+     * @param sc The Scanner object for user input.
      */
     public Input(Scanner sc) {
         this.sc = Objects.requireNonNullElseGet(sc, () -> new Scanner(System.in));
-
     }
 
+    /**
+     * Gets the underlying Scanner object.
+     *
+     * @return The Scanner object.
+     */
     public Scanner getScanner() {
         return this.sc;
     }
 
     /**
-     * Method to display a message and ask for integer input
-     * @param msgToPrint Message to be displayed
-     * @return ask for user input
+     * Asks the user for an integer input based on the provided message.
+     *
+     * @param msgToPrint The message to display to the user.
+     * @return The user-entered integer.
      */
     public Integer getInt(String msgToPrint) {
         String res;
@@ -52,11 +55,11 @@ public class Input {
         return newInt;
     }
 
-
     /**
-     * Method to display a message and ask for double input
-     * @param msgToPrint Message to be displayed
-     * @return ask for user input
+     * Asks the user for a double input based on the provided message.
+     *
+     * @param msgToPrint The message to display to the user.
+     * @return The user-entered double.
      */
     public double getDouble(String msgToPrint) {
         do {
@@ -69,17 +72,23 @@ public class Input {
         } while (true);
     }
 
-
     /**
-     * Method to display a message and ask for String input
-     * @param msgToPrint Message to be displayed
-     * @return ask for user input
+     * Asks the user for a String input based on the provided message.
+     *
+     * @param msgToPrint The message to display to the user.
+     * @return The user-entered String.
      */
     public String getLine(String msgToPrint) {
         System.out.print(msgToPrint);
         return this.sc.nextLine();
     }
 
+    /**
+     * Asks the user for a boolean input based on the provided message.
+     *
+     * @param msgToPrint The message to display to the user.
+     * @return The user-entered boolean.
+     */
     public boolean getBoolean(String msgToPrint) {
         System.out.print(msgToPrint);
         System.out.print("\n");
@@ -96,6 +105,12 @@ public class Input {
         }
     }
 
+    /**
+     * Asks the user for a date input in the specified format.
+     *
+     * @param msgToPrint The message to display to the user.
+     * @return The user-entered date.
+     */
     public Integer getDate(String msgToPrint) {
         System.out.print(msgToPrint);
         System.out.print("\n");
@@ -129,5 +144,4 @@ public class Input {
         }
         return year * 10000 + month * 100 + day;
     }
-
 }

42c3c30b1980093af29232e5b50dacd1df7c6489
leejyphoebe <87797355+leejyphoebe@users.noreply.github.com>
2023-11-26 23:06:04 +0800

    Update CampCMBoundary.java

--- CAMs/src/boundary/CampCMBoundary.java
+++ CAMs/src/boundary/CampCMBoundary.java
@@ -1,5 +1,14 @@
 package boundary;
 
+/**
+ * The CampCMBoundary class represents a camp committee member in the system.
+ * A camp committee member can be associated with many camps.
+ * This class provides methods for handling camp committee member operations such as viewing camps, registering for a camp, 
+ * submitting an enquiry, editing an enquiry, deleting an enquiry, and changing password.
+ * @author Group 2
+ * @since 2023-11-26
+ */
+
 import entity.*;
 import utils.GenerateReports.GenerateAttendeeReport;
 
@@ -7,10 +16,21 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class CampCMBoundary extends StudentBoundary{
+    /**
+     * Constructor for CampCMBoundary.
+     * @param centralManager The central manager controlling the system.
+     */
     public CampCMBoundary(CentralManager centralManager) {
         super(centralManager);
     }
 
+    /**
+     * This method handles the operations for the camp committee member.
+     * It displays a menu of choices and prompts the camp committee member to enter their choice.
+     * Depending on the camp committee member's choice, it calls the appropriate method to perform the chosen operation.
+     * If the camp committee member enters a choice that is not within the valid range (1-17), it prints an error message and prompts the camp committee member to enter their choice again.
+     * If the camp committee member chooses to exit the application, it prints a message indicating that the system is exiting.
+     */
     public void campCMOperations() {
         int choice = 0;
         while (choice != 17) {
@@ -47,6 +67,9 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * Displays the menu choices for the camp committee member.
+     */
     public void displayMenuChoices() {
         super.displayMenuChoices();
         System.out.print(
@@ -64,6 +87,9 @@ public class CampCMBoundary extends StudentBoundary{
         );
     }
 
+    /**
+     * Displays enquiries for the camp committee member's camp.
+     */
     public void viewCampEnquiries() {
         //view all enquiries to camp this student is campCM for
         ArrayList<Enquiry> relevantEnquiries = this.getEnquiryController().getEnquiryByCamp(this.getCampCMController().getCurrentCampCM().getCampID());
@@ -71,6 +97,9 @@ public class CampCMBoundary extends StudentBoundary{
         this.getEnquiryBoundary().printEnquiries(relevantEnquiries);
     }
 
+    /**
+     * Allows the camp committee member to reply to enquiries.
+     */
     public void replyEnquiries() {
         while (true) {
             //view all enquiries to camp this student is campCM for
@@ -88,17 +117,28 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * This method allows the camp committee member to submit a suggestion.
+     * It first retrieves the camp that the camp committee member is associated with.
+     * Then, it calls the `addSuggestion()` method of `SuggestionBoundary` to add the suggestion.
+     */
     public void submitSuggestion() {
         Camp camp = this.getCampController().getCampByID(this.getCampCMController().getCurrentCampCM().getCampID());
         this.getSuggestionBoundary().addSuggestion(camp, this.getCampCMController().getCurrentCampCM());
     }
 
+    /**
+     * Displays the camp committee member's suggestions.
+     */
     public void viewMySuggestions() {
         ArrayList<Suggestion> mySuggestions = this.getSuggestionController().getSuggestionsbyCampCM(this.getCampCMController().getCurrentCampCM().getUserID());
         System.out.println(".....................My Suggestions.....................");
         this.getSuggestionBoundary().printSuggestionList(mySuggestions);
     }
 
+    /**
+     * Allows the camp committee member to edit their suggestions.
+     */
     public void editMySuggestions() {
         while (true) {
             ArrayList<Suggestion> mySuggestions = this.getSuggestionController().getSuggestionsbyCampCM(this.getCampCMController().getCurrentCampCM().getUserID());
@@ -114,6 +154,9 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * Allows the camp committee member to delete their suggestions.
+     */
     public void deleteMySuggestions() {
         while (true) {
             ArrayList<Suggestion> mySuggestions = this.getSuggestionController().getSuggestionsbyCampCM(this.getCampCMController().getCurrentCampCM().getUserID());
@@ -134,6 +177,9 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * Generates an attendee list.
+     */
     public void generateAttendeeList() {
         String nameFilter, roleFilter;
         nameFilter = this.getLine("Enter filter for student names or just hit enter to skip");
@@ -155,12 +201,18 @@ public class CampCMBoundary extends StudentBoundary{
         generateReport.generateReport(fpath);
     }
 
+    /**
+     * Displays the camps that the camp committee member is eligible for.
+     */
     public void viewCamps() {
         // View available camps
         String studentID = this.getCampCMController().getCurrentCampCM().getUserID();
         this.getCampBoundary().viewCamps(studentID);
     }
 
+    /**
+     * Displays the camps that the camp committee member is associated with.
+     */
     public void viewMyCamps(){
         //View own camps
         ArrayList<Camp> myCamps = this.getCampCMController().getAttendedCamps();
@@ -172,6 +224,9 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * Allows the camp committee member to register for a camp.
+     */
     public void registerCamp() {
         // print all available camps
         while (true) {
@@ -202,6 +257,14 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * This method allows the camp committee member to withdraw from a camp.
+     * It first displays the camps that the camp committee member is associated with.
+     * Then, it prompts the camp committee member to enter the index of the camp they want to withdraw from.
+     * If the camp committee member enters -1, the method returns and no camp is withdrawn from.
+     * If the camp committee member enters a valid index, the chosen camp is withdrawn from, and a confirmation message is printed.
+     * If the camp cannot be withdrawn from because the camp committee member is the MainComm for the camp, an error message is printed.
+     */
     public void withdrawCamp(){
         while (true) {
             System.out.println("..........................My registered camps..........................");
@@ -232,6 +295,9 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * Allows the camp committee member to edit an enquiry.
+     */
     public void editEnquiry() {
         //Delete an enquiry for a camp
         while (true) {
@@ -263,6 +329,14 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * This method allows the camp committee member to submit an enquiry.
+     * It first displays the camps that the camp committee member is associated with.
+     * Then, it prompts the camp committee member to enter the index of the camp they want to enquire for.
+     * If the camp committee member enters -1, the method returns and no enquiry is submitted.
+     * If the camp committee member enters a valid index, the method prompts the camp committee member to enter their enquiry.
+     * The enquiry is then submitted, and a confirmation message is printed.
+     */
     public void submitEnquiry(){
         // Submit an enquiry for a camp
         while (true) {
@@ -280,6 +354,15 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * This method allows the camp committee member to delete an enquiry.
+     * It first prints a header, then retrieves the camp committee member's enquiries.
+     * If there are no enquiries, it prints a message indicating this and returns.
+     * Otherwise, it prints each enquiry in the list and prompts the camp committee member to enter the index of the enquiry they want to delete.
+     * If the camp committee member enters -1, the method returns and no enquiry is deleted.
+     * If the camp committee member enters a valid index, the chosen enquiry is deleted, and a confirmation message is printed.
+     * If the enquiry cannot be deleted because the camp committee member was not the sender or because it was already answered, an error message is printed.
+     */
     public void deleteEnquiry(){
         //Delete an enquiry for a camp
         while (true) {
@@ -310,6 +393,12 @@ public class CampCMBoundary extends StudentBoundary{
         }
     }
 
+    /**
+     * This method displays the camp committee member's enquiries.
+     * It first prints a header, then retrieves the camp committee member's enquiries.
+     * If there are no enquiries, it prints a message indicating this.
+     * Otherwise, it prints each enquiry in the list.
+     */
     public void viewMyEnquiries(){
         System.out.println("..............................My Enquiries..............................");
         ArrayList<Enquiry> myEnquiries = this.getCampCMController().getMyEnquiries();
